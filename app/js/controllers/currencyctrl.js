cfd3d46f2074b0a0d38b79e1bb585a7c8e751a61
roine <feunetre@hotmail.com>
2013-10-25 23:02:57 -0500

    can now convert

--- app/js/controllers/currencyctrl.js
+++ app/js/controllers/currencyctrl.js
@@ -2,19 +2,49 @@ define([], function() {
 	'use strict';
 	
 	return ['$scope', '$http', '$routeParams', 'jonCurrencies', function($scope, $http, $routeParams, jonCurrencies) {
+
+        // cache my values and return it if the normal result is wrong
+		var cache = {},
+        currencies,
+        result;
 		
+		function calculate(){
+            try{
+                cache.result = $scope.$eval(currencies.val) * currencies.rate || cache.result;
+                currencies.result = cache.result;
+            }
+            catch(e){
+                result = cache.result;            
+            }
+        }
+
+		currencies = $routeParams;
+
+
 		// Load the currencies
 		jonCurrencies.get()
 		.then(function(data){
-			$scope.currencies = data;
-			$scope.currency = $routeParams;
+			currencies.all = data;
 		}, function(error){
 			console.log(error);
 		});
 
-		$scope.switch = function(){
+		
+		jonCurrencies
+        .convert(currencies.from, currencies.to)
+        .then(function(data){
+			currencies.rate = data.rate;
+			cache.result = currencies.val * data.rate || cache.result;
+			currencies.result = cache.result;
+        }, function(error){
+            // jonAlert.add({type: 'error', msg: error});
+            result = cache.result || 1;
+        });
 
-		};
+        $scope.currencies = currencies;
+        $scope.result = result;
+
+        $scope.$watch('currencies.val', calculate);
 		// because this has happened asynchroneusly we've missed
 		// Angular's initial call to $apply after the controller has been loaded
 		// hence we need to explicityly call it at the end of our Controller constructor
